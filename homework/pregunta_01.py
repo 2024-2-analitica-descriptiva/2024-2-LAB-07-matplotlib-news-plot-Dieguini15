08ce63f532dac1665e4cea9482d060b3e854f383
Dieguini15 <jdquinteroc@unal.edu.co>
2025-01-15 20:25:21 -0500

    Lab 3

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -4,15 +4,22 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 
 # pylint: disable=import-outside-toplevel
 
-
+import pandas as pd
+import matplotlib.pyplot as plt
 def pregunta_01():
-    """
-    Siga las instrucciones del video https://youtu.be/qVdwpxG_JpE para
-    generar el archivo `files/plots/news.png`.
+    plt.figure()
+    df=pd.read_csv("news.csv",index_col=0)
+    for col in df.columns:
+        plt.plot(df[col],label=col)
+    plt.show()
+pregunta_01()
+"""
+Siga las instrucciones del video https://youtu.be/qVdwpxG_JpE para
+generar el archivo `files/plots/news.png`.
 
-    Un ejemplo de la grafica final esta ubicado en la raíz de
-    este repo.
+Un ejemplo de la grafica final esta ubicado en la raíz de
+este repo.
 
-    El gráfico debe salvarse al archivo `files/plots/news.png`.
+El gráfico debe salvarse al archivo `files/plots/news.png`.
 
-    """
+"""
